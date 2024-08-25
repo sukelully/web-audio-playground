c7448854c7ce00ace2e7bb2bb73ac50f37fbc4d2
sukelully <sully097@gmail.com>
2024-08-25 12:56:29 +0100

    Refactor

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 const playNoiseBtn = document.getElementById('play-noise');
 
+// Global variables
 let audioContext;
+let gainNode;
 
-playNoise = () => {
-    if (!audioContext) startAudioContext();
+// Function to start the audio context
+const startAudioContext = () => {
+    if (!audioContext) {
+        audioContext = new AudioContext();
+        console.log(`Audio context started with sample rate: ${audioContext.sampleRate}`);
+    }
+}
 
-    var bufferSize = 2 * audioContext.sampleRate,
-        noiseBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate),
-        output = noiseBuffer.getChannelData(0);
-    for (var i = 0; i < bufferSize; i++) {
+// Function to set up the synth
+const setupSynth = () => {
+    startAudioContext();
+    if (!gainNode) {
+        gainNode = audioContext.createGain();
+        gainNode.connect(audioContext.destination);
+        console.log('Synth setup complete with gain node connected.');
+    }
+}
+
+// Function to play white noise
+const playNoise = () => {
+    setupSynth();
+
+    const bufferSize = 2 * audioContext.sampleRate;
+    const noiseBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
+    const output = noiseBuffer.getChannelData(0);
+    
+    // Fill the buffer with random noise
+    for (let i = 0; i < bufferSize; i++) {
         output[i] = Math.random() * 2 - 1;
     }
 
-    var whiteNoise = audioContext.createBufferSource();
+    const whiteNoise = audioContext.createBufferSource();
     whiteNoise.buffer = noiseBuffer;
     whiteNoise.loop = true;
-    whiteNoise.connect(audioContext.destination);
+    whiteNoise.connect(gainNode);
 
     whiteNoise.start();
     whiteNoise.stop(audioContext.currentTime + 0.1);
 }
 
-startAudioContext = () => {
-    audioContext = new AudioContext();
-    console.log(audioContext.sampleRate);
-}
-
+// Attach the event listener
 playNoiseBtn.addEventListener('click', playNoise);
