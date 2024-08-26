1b2e4f34da9c74bbdb738fba4d662b02ac2f2751
sukelully <sully097@gmail.com>
2024-08-26 13:13:43 +0100

    All four sliders working

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const feedbackSlider = document.getElementById('feedback-slider');
 const feedbackValue = document.getElementById('feedback-value');
 const freqSlider = document.getElementById('freq-slider');
 const freqValue = document.getElementById('freq-value');
-
+const qSlider = document.getElementById('q-slider');
+const qValue = document.getElementById('q-value');
 
 // Global variables
 let audioContext;
@@ -23,7 +24,7 @@ const startAudioContext = () => {
     }
 }
 
-// Function to set up the synth
+// Setup synth, connect nodes
 const setupSynth = () => {
     startAudioContext();
     if (!delayNode) {
@@ -34,13 +35,13 @@ const setupSynth = () => {
 
         delayNode.delayTime.value = 0.001;  // Adjusted delay time (e.g., 20ms for a mid-range pitch)
         gainNode.gain.value = 0.4;
-        feedbackNode.gain.value = 0.7;
+        feedbackNode.gain.value = 0.8;
         filterNode.type = 'lowpass';
-        filterNode.frequency.value = 500; 
-        filterNode.Q.value = 0.9;
+        filterNode.frequency = 500; 
+        filterNode.Q.value = 0.0001;
 
         // Connect nodes
-        gainNode.connect(audioContext.destination);
+        // gainNode.connect(audioContext.destination);
         gainNode.connect(delayNode);
         delayNode.connect(feedbackNode);
         feedbackNode.connect(filterNode);
@@ -51,7 +52,7 @@ const setupSynth = () => {
     }
 }
 
-// Function to play white noise
+// Play white noise burst
 const playNoise = () => {
     setupSynth();
 
@@ -73,9 +74,6 @@ const playNoise = () => {
     noiseNode.stop(audioContext.currentTime + 0.01); 
 }
 
-// Event listeners
-playNoiseBtn.addEventListener('click', playNoise);
-
 delaySlider.oninput = function() {
     delayValue.innerHTML = this.value;
     delayNode.delayTime.value = this.value;
@@ -88,5 +86,27 @@ feedbackSlider.oninput = function() {
 
 freqSlider.oninput = function() {
     freqValue.innerHTML = this.value;
-    filterNode.frequency.value = this.value;
+    filterNode.frequency = this.value;
 }
+
+qSlider.oninput = function() {
+    qValue.innerHTML = this.value;
+    filterNode.Q.value = this.value;
+}
+
+const updateSliderValues = () => {
+    delayValue.innerHTML = delaySlider.value;
+    feedbackValue.innerHTML = feedbackSlider.value;
+    freqValue.innerHTML = freqSlider.value;
+    qValue.innerHTML = qSlider.value;
+}
+
+// Play noise when button is clicked or spacebar is pressed
+playNoiseBtn.addEventListener('click', playNoise);
+window.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
+        playNoise();
+    }
+});
+
+window.addEventListener('DOMContentLoaded', updateSliderValues);
