0111d8e7740bb687cf4cd6526cad57a8fcc32372
sukelully <sully097@gmail.com>
2024-08-26 13:26:34 +0100

    Praying this works

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Global variables
+let audioContext;
+let noiseNode
+let gainNode;
+let delayNode;
+let filterNode;
+let feedbackNode;
+
+// References
 const playNoiseBtn = document.getElementById('play-noise');
 const delaySlider = document.getElementById('delay-slider');
 const delayValue = document.getElementById('delay-value');
@@ -8,59 +17,47 @@ const freqValue = document.getElementById('freq-value');
 const qSlider = document.getElementById('q-slider');
 const qValue = document.getElementById('q-value');
 
-// Global variables
-let audioContext;
-let noiseNode
-let gainNode;
-let delayNode;
-let filterNode;
-let feedbackNode;
+// Start audio context, connect nodes
+const setupAudio = () => {
+    // Start audio context and log device sample rate
+    audioContext = new AudioContext();
+    console.log(`Audio context started with sample rate: ${audioContext.sampleRate}`);
 
-// Function to start the audio context
-const startAudioContext = () => {
-    if (!audioContext) {
-        audioContext = new AudioContext();
-        console.log(`Audio context started with sample rate: ${audioContext.sampleRate}`);
-    }
-}
+    // Initialise K-S algorithm nodes
+    delayNode = audioContext.createDelay();
+    gainNode = audioContext.createGain();
+    feedbackNode = audioContext.createGain();
+    filterNode = audioContext.createBiquadFilter();
 
-// Setup synth, connect nodes
-const setupSynth = () => {
-    startAudioContext();
-    if (!delayNode) {
-        delayNode = audioContext.createDelay();
-        gainNode = audioContext.createGain();
-        feedbackNode = audioContext.createGain();
-        filterNode = audioContext.createBiquadFilter();
+    // Set default algorithm values
+    delayNode.delayTime.value = 0.001;
+    gainNode.gain.value = 0.4;
+    feedbackNode.gain.value = 0.8;
+    filterNode.type = 'lowpass';
+    filterNode.frequency = 500;
+    filterNode.Q.value = 0.0001;
 
-        delayNode.delayTime.value = 0.001;  // Adjusted delay time (e.g., 20ms for a mid-range pitch)
-        gainNode.gain.value = 0.4;
-        feedbackNode.gain.value = 0.8;
-        filterNode.type = 'lowpass';
-        filterNode.frequency = 500; 
-        filterNode.Q.value = 0.0001;
+    // Routes nodes
+    gainNode.connect(delayNode);
+    delayNode.connect(feedbackNode);
+    feedbackNode.connect(filterNode);
+    filterNode.connect(delayNode);
+    filterNode.connect(audioContext.destination);
 
-        // Connect nodes
-        // gainNode.connect(audioContext.destination);
-        gainNode.connect(delayNode);
-        delayNode.connect(feedbackNode);
-        feedbackNode.connect(filterNode);
-        filterNode.connect(delayNode);
-        filterNode.connect(audioContext.destination);
-
-        feedbackNode.connect(audioContext.destination);
-    }
+    feedbackNode.connect(audioContext.destination);
 }
 
 // Play white noise burst
 const playNoise = () => {
-    setupSynth();
+    // Start audio context if there isn't an instance already
+    if (!audioContext) {
+        setupAudio();
+    }
 
+    // Create a buffer and fill with random noise values
     const bufferSize = 2 * audioContext.sampleRate;
     const noiseBuffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
     const output = noiseBuffer.getChannelData(0);
-    
-    // Fill the buffer with random noise
     for (let i = 0; i < bufferSize; i++) {
         output[i] = Math.random() * 2 - 1;
     }
@@ -71,25 +68,25 @@ const playNoise = () => {
     noiseNode.connect(gainNode);
 
     noiseNode.start();
-    noiseNode.stop(audioContext.currentTime + 0.01); 
+    noiseNode.stop(audioContext.currentTime + 0.01);
 }
 
-delaySlider.oninput = function() {
+delaySlider.oninput = function () {
     delayValue.innerHTML = this.value;
     delayNode.delayTime.value = this.value;
 }
 
-feedbackSlider.oninput = function() {
+feedbackSlider.oninput = function () {
     feedbackValue.innerHTML = this.value;
     feedbackNode.gain.value = this.value;
 }
 
-freqSlider.oninput = function() {
+freqSlider.oninput = function () {
     freqValue.innerHTML = this.value;
     filterNode.frequency = this.value;
 }
 
-qSlider.oninput = function() {
+qSlider.oninput = function () {
     qValue.innerHTML = this.value;
     filterNode.Q.value = this.value;
 }
